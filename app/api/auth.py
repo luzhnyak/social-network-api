c0d8a41b8fb8491ba3dba3cce000fd0acc10290b
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-12-13 18:04:59 +0200

    add CORS

--- app/api/auth.py
+++ app/api/auth.py
@@ -7,12 +7,13 @@ from app.schemas.auth import UserCreate
 from app.core.security import hash_password, verify_password
 from app.core.jwt import create_access_token
 from app.schemas.token import Token
+from app.deps import get_current_user, get_db
 
 router = APIRouter()
 
 
 @router.post("/register")
-def register(user: UserCreate, db: Session = Depends(SessionLocal)):
+def register(user: UserCreate, db: Session = Depends(get_db)):
     if db.query(User).filter(User.email == user.email).first():
         raise HTTPException(status_code=400, detail="Email already registered")
     new_user = User(email=user.email,
@@ -24,7 +25,7 @@ def register(user: UserCreate, db: Session = Depends(SessionLocal)):
 
 
 @router.post("/login", response_model=Token)
-def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(SessionLocal)):
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(User).filter(User.email == form_data.username).first()
     if not user or not verify_password(form_data.password, user.hashed_password):
         raise HTTPException(
--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.db import Base, engine
 from app.api import auth, telegram
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Дозволити CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Додайте домен вашого фронтенду
+    allow_credentials=True,
+    allow_methods=["*"],  # Дозволити всі HTTP-методи
+    allow_headers=["*"],  # Дозволити всі заголовки
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(telegram.router, prefix="/telegram", tags=["Telegram"])
