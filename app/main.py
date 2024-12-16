b96f2560c43bb2e55db4bc7cd14460e83cef7ff5
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-12-16 11:36:19 +0200

    deploy

--- app/main.py
+++ app/main.py
@@ -21,3 +21,8 @@ app.add_middleware(
 
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(telegram.router, prefix="/telegram", tags=["Telegram"])
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
